45633106574c0e5c87e4cb20d63e79c329c53af9
Correia Lopes Dylan <dylan.correia@hotmail.com>
2019-04-28 16:06:50 +0200

    create count and delete items

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_173644) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_04_23_131542) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
