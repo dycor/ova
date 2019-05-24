8203d5c273beb7ab02b1f5bd6911ffd6f38d87b4
Correia Lopes Dylan <dylan.correia@hotmail.com>
2019-05-24 16:48:06 +0200

    add name and lastname

--- api/app/commands/authenticate_user.rb
+++ api/app/commands/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    JsonWebToken.encode(user_id: user.id,firstname: user.firstname, name: user.name) if user
   end
 
   private
