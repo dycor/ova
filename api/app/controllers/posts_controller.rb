524d12b54c0cbea905e94be7a2859748a8c98a15
amandineripoll <32871125+amandineripoll@users.noreply.github.com>
2019-07-16 22:00:05 +0200

    Feat crud article (#24)
    * Create controller and model + update route file
    
    * Posts crud done
    
    * posts_crud
    
    * add comments
    
    * change autorization
    
    *  delete useless file

---
+++ api/app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+	skip_before_action :authenticate_request, only: [:index, :show, :get_comments]
+	before_action :set_post, only: [:update, :destroy]
+
+	# GET /posts
+	def index
+		@posts = Post.all
+
+		render json: @posts, status: :success #200
+	end
+
+	#GET /posts/id
+	def show
+		render json: @post
+	end
+
+	#POST /posts
+	def create
+
+		@post = Post.new(post_params)
+
+		if @post.save
+			render json: @post, status: :created, location: @post
+		else
+			render json: @post.errors, status: :bad_request #400
+		end
+	end
+
+
+	#PATH / PUT / posts/:id
+	def update
+		if @post.update(post_params)
+			render json: @post, status: :success
+		else
+			render json: @post.errors, status: :unprocessable_entity #422
+		end
+	end
+
+	#DELETE /posts/:id
+
+	def destroy
+		@post.destroy
+    end
+
+	# GET /posts/:id/comments
+	def get_comments
+		render json: @post.comments
+	end
+
+
+    def set_post
+		@post = Post.find(params[:id])
+	end
+
+    def post_params
+		params.permit(:title, :content, :image, :tag_list)
+	end
+end
